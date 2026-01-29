bd6930ad426b0f7033be8944fc594d5edadf612b
SimpleStew007 <daniellopezgerardo02e@gmail.com>
2026-01-28 18:09:54 -0800

    Build: nether.js

--- logros/nether.js
+++ logros/nether.js
@@ -1,8 +1,9 @@
-registrarLogro({
-    id: "dimension_roja",
-    titulo: "¡Necesitamos Profundizar!",
-    descripcion: "Construye y atraviesa un portal de Obsidiana al Nether.",
-    tipo: "Exploración/Dimensional",
-    requisitos: "Encender un portal al Nether y cruzarlo.",
-    ediciones: ["java", "bedrock"]
-});
+/** Este archivo simula la estructura de un archivo de detalle que se cargaría para una vista de logro individual. */
+window.logroData = {
+    id: 'un_viaje_obligado',
+    name: 'Un viaje obligado',
+    desc: 'Entra a la dimensión del Nether. ¡Ten cuidado!',
+    type: 'Java/Bedrock',
+    prerequisito: 'Obsidiana y pedernal',
+    guia: 'Necesitas 14 bloques de Obsidiana para construir el marco y encenderlo con un mechero (pedernal y hierro).'
+};
