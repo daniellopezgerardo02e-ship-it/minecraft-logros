6d7d65071487b372117dd4578632ce8e0c3bffaa
SimpleStew007 <daniellopezgerardo02e@gmail.com>
2026-01-28 18:09:52 -0800

    Build: pocion.js

---
+++ logros/pocion.js
@@ -0,0 +1,9 @@
+/** Este archivo simula la estructura de un archivo de detalle que se cargaría para una vista de logro individual. */
+window.logroData = {
+    id: 'invisible_mob',
+    name: 'Arrojando la poción',
+    desc: 'Haz invisible a un enemigo arrojándole una Poción de Invisibilidad (solo Bedrock).',
+    type: 'Bedrock',
+    prerequisito: 'Creación de pociones',
+    guia: 'Necesitas pociones arrojadizas (Splash Potions) y polvo de piedra roja para el crafteo final.'
+};
