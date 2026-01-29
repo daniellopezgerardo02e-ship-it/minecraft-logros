0701bc130c53b5170378d70c4b951f8dae7575de
SimpleStew007 <daniellopezgerardo02e@gmail.com>
2026-01-28 17:26:50 -0800

    Auto-Wiki: El Artillero (106) (bedrock)

---
+++ logros/bedrock/el_artillero_(106).js
@@ -0,0 +1,13 @@
+window.logroData = {
+    "name": "El Artillero (106)",
+    "icono": "crossbow",
+    "categoria": "Combate",
+    "inventoryNed": [
+        "crossbow"
+    ],
+    "tutorial": [
+        "Mata a 50 mobs hostiles usando la ballesta"
+    ],
+    "proTip": "Usa la ballesta para flechas de fuegos artificiales (Rocket Man).",
+    "edition": "bedrock"
+};
