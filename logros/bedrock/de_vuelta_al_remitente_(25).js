a2ebf3ced593757d6c5461feb83bc500bb4bb12b
SimpleStew007 <daniellopezgerardo02e@gmail.com>
2026-01-28 17:26:04 -0800

    Auto-Wiki: De Vuelta al Remitente (25) (bedrock)

---
+++ logros/bedrock/de_vuelta_al_remitente_(25).js
@@ -0,0 +1,13 @@
+window.logroData = {
+    "name": "De Vuelta al Remitente (25)",
+    "icono": "flame",
+    "categoria": "Peligro",
+    "inventoryNed": [
+        "fire_charge"
+    ],
+    "tutorial": [
+        "Mata un Ghast en el Nether usando su propia bola de fuego (desviándola)"
+    ],
+    "proTip": "Requiere un 'timing' preciso con la espada o la mano desnuda.",
+    "edition": "bedrock"
+};
