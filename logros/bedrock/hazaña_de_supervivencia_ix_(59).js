ae6002524576f3dad209ef1c0923287642cfb57f
SimpleStew007 <daniellopezgerardo02e@gmail.com>
2026-01-28 17:26:23 -0800

    Auto-Wiki: Hazaña de Supervivencia IX (59) (bedrock)

---
+++ logros/bedrock/hazaña_de_supervivencia_ix_(59).js
@@ -0,0 +1,13 @@
+window.logroData = {
+    "name": "Hazaña de Supervivencia IX (59)",
+    "icono": "zap",
+    "categoria": "Progresión",
+    "inventoryNed": [
+        "experience_bottle"
+    ],
+    "tutorial": [
+        "Alcanza el nivel 45 de experiencia"
+    ],
+    "proTip": "Prepara pociones de resistencia para el combate avanzado.",
+    "edition": "bedrock"
+};
