64ae26a2f1affd5860eb27dc4217c4a01225c90a
SimpleStew007 <daniellopezgerardo02e@gmail.com>
2026-01-28 17:26:06 -0800

    Auto-Wiki: Un Viaje de Ida (29) (bedrock)

---
+++ logros/bedrock/un_viaje_de_ida_(29).js
@@ -0,0 +1,15 @@
+window.logroData = {
+    "name": "Un Viaje de Ida (29)",
+    "icono": "castle",
+    "categoria": "Exploración",
+    "inventoryNed": [
+        "eye_of_ender"
+    ],
+    "tutorial": [
+        "Viaja al Nether",
+        "Crea Ojos de Ender",
+        "Sigue los Ojos hasta una Fortaleza"
+    ],
+    "proTip": "Asegúrate de llevar suficiente comida y pociones de resistencia al fuego.",
+    "edition": "bedrock"
+};
