96aaa8b297ad194db5a0a1eb506a9cfc5492d80b
SimpleStew007 <daniellopezgerardo02e@gmail.com>
2026-01-28 17:26:15 -0800

    Auto-Wiki: Explosión Controlada (45) (bedrock)

---
+++ logros/bedrock/explosión_controlada_(45).js
@@ -0,0 +1,14 @@
+window.logroData = {
+    "name": "Explosión Controlada (45)",
+    "icono": "bomb",
+    "categoria": "Redstone",
+    "inventoryNed": [
+        "tnt",
+        "redstone_torch"
+    ],
+    "tutorial": [
+        "Detona un bloque de TNT usando Redstone"
+    ],
+    "proTip": "El TNT es útil para la minería a gran escala.",
+    "edition": "bedrock"
+};
