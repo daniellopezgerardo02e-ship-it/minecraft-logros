096619501efad6a7ba01019703250bf72dfc9331
SimpleStew007 <daniellopezgerardo02e@gmail.com>
2026-01-28 17:26:44 -0800

    Auto-Wiki: Coleccionista de Flores IV (95) (bedrock)

---
+++ logros/bedrock/coleccionista_de_flores_iv_(95).js
@@ -0,0 +1,13 @@
+window.logroData = {
+    "name": "Coleccionista de Flores IV (95)",
+    "icono": "flower",
+    "categoria": "Estética",
+    "inventoryNed": [
+        "oxeye_daisy"
+    ],
+    "tutorial": [
+        "Recolecta una Margarita Ojo de Buey"
+    ],
+    "proTip": "Común en llanuras.",
+    "edition": "bedrock"
+};
