971b4bdb9cd91614fb98d9480204f48e7c233f6d
SimpleStew007 <daniellopezgerardo02e@gmail.com>
2026-01-28 17:26:50 -0800

    Auto-Wiki: Gran Breaker (107) (bedrock)

---
+++ logros/bedrock/gran_breaker_(107).js
@@ -0,0 +1,13 @@
+window.logroData = {
+    "name": "Gran Breaker (107)",
+    "icono": "shield",
+    "categoria": "Defensa",
+    "inventoryNed": [
+        "shield"
+    ],
+    "tutorial": [
+        "Bloquea 500 puntos de daño con el escudo"
+    ],
+    "proTip": "El escudo se rompe, pero puede ser reparado con madera.",
+    "edition": "bedrock"
+};
