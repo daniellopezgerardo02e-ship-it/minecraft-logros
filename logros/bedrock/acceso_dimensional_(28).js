36b81c2258f7e023a4200a04f4320686418b355a
SimpleStew007 <daniellopezgerardo02e@gmail.com>
2026-01-28 17:26:06 -0800

    Auto-Wiki: Acceso Dimensional (28) (bedrock)

---
+++ logros/bedrock/acceso_dimensional_(28).js
@@ -0,0 +1,15 @@
+window.logroData = {
+    "name": "Acceso Dimensional (28)",
+    "icono": "portal",
+    "categoria": "Viaje",
+    "inventoryNed": [
+        "obsidian",
+        "flint_and_steel"
+    ],
+    "tutorial": [
+        "Mina Obsidiana (necesitas Diamante)",
+        "Construye y enciende un portal al Nether"
+    ],
+    "proTip": "Usa el Nether para viajar largas distancias en el mundo superior (1 bloque = 8 bloques).",
+    "edition": "bedrock"
+};
