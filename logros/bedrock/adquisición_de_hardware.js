89bed7ca6ef0c5400111406ae12a52bf6a85b948
SimpleStew007 <daniellopezgerardo02e@gmail.com>
2026-01-28 17:24:09 -0800

    Auto-Wiki: Adquisición de Hardware (bedrock)

---
+++ logros/bedrock/adquisición_de_hardware.js
@@ -0,0 +1,13 @@
+window.logroData = {
+    "name": "Adquisición de Hardware",
+    "icono": "ingot",
+    "categoria": "Fundición y Minerales",
+    "inventoryNed": [
+        "Lingote de Hierro"
+    ],
+    "tutorial": [
+        "1. Funde Mena de Hierro en un Horno (Ver 'Tema Candente')."
+    ],
+    "proTip": "El hierro es esencial para herramientas duraderas, especialmente el pico, necesario para minar materiales más valiosos.",
+    "edition": "bedrock"
+};
