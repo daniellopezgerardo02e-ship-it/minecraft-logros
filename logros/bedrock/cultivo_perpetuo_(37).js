a9c8b68288b1d65045cfae0f598ff076d622d763
SimpleStew007 <daniellopezgerardo02e@gmail.com>
2026-01-28 17:26:11 -0800

    Auto-Wiki: Cultivo Perpetuo (37) (bedrock)

---
+++ logros/bedrock/cultivo_perpetuo_(37).js
@@ -0,0 +1,14 @@
+window.logroData = {
+    "name": "Cultivo Perpetuo (37)",
+    "icono": "wheat",
+    "categoria": "Agricultura",
+    "inventoryNed": [
+        "seed",
+        "bone_meal"
+    ],
+    "tutorial": [
+        "Crea una granja automatizada de trigo o caña de azúcar"
+    ],
+    "proTip": "Usa observadores y pistones para la automatización avanzada.",
+    "edition": "bedrock"
+};
