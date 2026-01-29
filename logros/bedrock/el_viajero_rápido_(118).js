ad8fe1d18bac208127d6c6aee90eb3568eefd87b
SimpleStew007 <daniellopezgerardo02e@gmail.com>
2026-01-28 17:26:56 -0800

    Auto-Wiki: El Viajero Rápido (118) (bedrock)

---
+++ logros/bedrock/el_viajero_rápido_(118).js
@@ -0,0 +1,14 @@
+window.logroData = {
+    "name": "El Viajero Rápido (118)",
+    "icono": "rail-symbol",
+    "categoria": "Viaje",
+    "inventoryNed": [
+        "powered_rail",
+        "minecart"
+    ],
+    "tutorial": [
+        "Construye un sistema de vías de 1,000 bloques y úsalo"
+    ],
+    "proTip": "Las vías propulsadas requieren bloques de Redstone o palancas.",
+    "edition": "bedrock"
+};
