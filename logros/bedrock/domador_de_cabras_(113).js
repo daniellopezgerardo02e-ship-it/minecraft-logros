65ac73a4104b060b300c1a13e439a9f86e280f40
SimpleStew007 <daniellopezgerardo02e@gmail.com>
2026-01-28 17:26:54 -0800

    Auto-Wiki: Domador de Cabras (113) (bedrock)

---
+++ logros/bedrock/domador_de_cabras_(113).js
@@ -0,0 +1,13 @@
+window.logroData = {
+    "name": "Domador de Cabras (113)",
+    "icono": "mountain",
+    "categoria": "Mobs",
+    "inventoryNed": [
+        "goat_horn"
+    ],
+    "tutorial": [
+        "Haz que una cabra embista a un bloque"
+    ],
+    "proTip": "Las cabras solo se encuentran en biomas de montañas extremas.",
+    "edition": "bedrock"
+};
