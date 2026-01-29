3fdad8cd72567e51fb59a9a843c59e59f7bea4c5
SimpleStew007 <daniellopezgerardo02e@gmail.com>
2026-01-28 17:26:30 -0800

    Auto-Wiki: Explorador de Biomas (70) (bedrock)

---
+++ logros/bedrock/explorador_de_biomas_(70).js
@@ -0,0 +1,13 @@
+window.logroData = {
+    "name": "Explorador de Biomas (70)",
+    "icono": "tree-pine",
+    "categoria": "Viaje",
+    "inventoryNed": [
+        "map"
+    ],
+    "tutorial": [
+        "Descubre un bioma de Cherry Grove"
+    ],
+    "proTip": "Busca la nueva madera de cerezo.",
+    "edition": "bedrock"
+};
