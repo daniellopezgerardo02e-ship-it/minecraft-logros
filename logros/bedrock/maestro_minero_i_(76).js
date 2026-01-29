a769ceda86ccbc2b17a5f5da4286d4f3ace4650c
SimpleStew007 <daniellopezgerardo02e@gmail.com>
2026-01-28 17:26:33 -0800

    Auto-Wiki: Maestro Minero I (76) (bedrock)

---
+++ logros/bedrock/maestro_minero_i_(76).js
@@ -0,0 +1,13 @@
+window.logroData = {
+    "name": "Maestro Minero I (76)",
+    "icono": "pickaxe",
+    "categoria": "Minería",
+    "inventoryNed": [
+        "coal"
+    ],
+    "tutorial": [
+        "Mina 1000 minerales de Carbón"
+    ],
+    "proTip": "El carbón es abundante en cuevas superficiales.",
+    "edition": "bedrock"
+};
