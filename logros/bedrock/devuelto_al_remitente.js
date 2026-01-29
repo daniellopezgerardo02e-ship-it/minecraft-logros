15c9df9c89848e60c8422d9e60b91739181ea5b7
SimpleStew007 <daniellopezgerardo02e@gmail.com>
2026-01-28 17:24:17 -0800

    Auto-Wiki: Devuelto al Remitente (bedrock)

---
+++ logros/bedrock/devuelto_al_remitente.js
@@ -0,0 +1,15 @@
+window.logroData = {
+    "name": "Devuelto al Remitente",
+    "icono": "ghost",
+    "categoria": "Nether y Combate",
+    "inventoryNed": [
+        "Estar en el Nether"
+    ],
+    "tutorial": [
+        "1. Encuentra un Ghast en el Nether.",
+        "2. Espera a que dispare una bola de fuego.",
+        "3. Golpea la bola de fuego (con cualquier herramienta o mano) para devolverla y matar al Ghast."
+    ],
+    "proTip": "Acércate a él en campo abierto. El momento clave es justo después de que el Ghast abra la boca y antes de que la bola de fuego viaje demasiado lejos.",
+    "edition": "bedrock"
+};
