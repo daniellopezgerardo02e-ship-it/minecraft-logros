0a42dc015aee2cd5d92524592eb1185f7a3b6ef6
SimpleStew007 <daniellopezgerardo02e@gmail.com>
2026-01-28 17:18:14 -0800

    Wiki Logro: El Último Suspiro (Matar al Wither)

---
+++ logros/el_último_suspiro_(matar_al_wither).js
@@ -0,0 +1,8 @@
+window.currentLogro = {
+    name: "El Último Suspiro (Matar al Wither)",
+    icono: "skull",
+    categoria: "Jefes",
+    inventoryNed: ["4x Arena de Almas (Soul Sand)","3x Cráneos de Esqueleto Wither (Wither Skeleton Skulls)","Armadura de Netherita/Diamante (encantada con Protección IV)","Espada con Castigo V (Smite V)","Pociones de Fuerza II","Curación II","Arco y flechas (preferiblemente con Infinito)"],
+    tutorial: ["1. Viaja al Nether y recoge Arena de Almas (Soul Sand) y caza los raros Esqueletos Wither en la Fortaleza del Nether para obtener 3 cráneos.","2. Prepara una arena de combate sellada. Una cueva grande de Bedrock (si estás en el mundo superior) o un área subterránea hecha de Obsidiana (para contener la explosión inicial) es ideal.","3. Coloca la Arena de Almas en forma de 'T' y luego los 3 Cráneos en la parte superior para invocar al jefe.","4. Durante la primera mitad de la batalla","el Wither vuela. Usa el arco o ballesta. Mantente en movimiento para esquivar sus proyectiles.","5. Cuando su salud baje a la mitad","el Wither se pondrá un escudo que lo hace inmune a las flechas. En este punto","atácalo cuerpo a cuerpo con la espada (Castigo V)."],
+    proTip: "El encantamiento Castigo (Smite) hace un daño masivo al Wither, ya que es una criatura no muerta. Lleva siempre cubos de leche para eliminar el efecto de Desecación (Wither)."
+};
