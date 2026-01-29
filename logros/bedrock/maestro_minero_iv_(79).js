1617d2578b3549a00c2f5e11c6e7280a8fa5d4b6
SimpleStew007 <daniellopezgerardo02e@gmail.com>
2026-01-28 17:26:35 -0800

    Auto-Wiki: Maestro Minero IV (79) (bedrock)

---
+++ logros/bedrock/maestro_minero_iv_(79).js
@@ -0,0 +1,13 @@
+window.logroData = {
+    "name": "Maestro Minero IV (79)",
+    "icono": "pickaxe",
+    "categoria": "Minería",
+    "inventoryNed": [
+        "redstone_dust"
+    ],
+    "tutorial": [
+        "Mina 300 minerales de Redstone"
+    ],
+    "proTip": "Se encuentra en grandes vetas.",
+    "edition": "bedrock"
+};
