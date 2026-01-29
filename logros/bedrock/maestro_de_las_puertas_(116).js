ca4eaaa0a8eed427911b6e70723c205dad8d7b4b
SimpleStew007 <daniellopezgerardo02e@gmail.com>
2026-01-28 17:26:55 -0800

    Auto-Wiki: Maestro de las Puertas (116) (bedrock)

---
+++ logros/bedrock/maestro_de_las_puertas_(116).js
@@ -0,0 +1,14 @@
+window.logroData = {
+    "name": "Maestro de las Puertas (116)",
+    "icono": "door-open",
+    "categoria": "Redstone",
+    "inventoryNed": [
+        "piston",
+        "pressure_plate"
+    ],
+    "tutorial": [
+        "Crea y usa una puerta automática con pistones y placas de presión"
+    ],
+    "proTip": "Asegúrate de que la redstone esté configurada correctamente.",
+    "edition": "bedrock"
+};
