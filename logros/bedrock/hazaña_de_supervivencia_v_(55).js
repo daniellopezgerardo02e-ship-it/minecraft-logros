2ecaf7e756ba12dd6fa317158247ba930a68b0dd
SimpleStew007 <daniellopezgerardo02e@gmail.com>
2026-01-28 17:26:21 -0800

    Auto-Wiki: Hazaña de Supervivencia V (55) (bedrock)

---
+++ logros/bedrock/hazaña_de_supervivencia_v_(55).js
@@ -0,0 +1,13 @@
+window.logroData = {
+    "name": "Hazaña de Supervivencia V (55)",
+    "icono": "zap",
+    "categoria": "Progresión",
+    "inventoryNed": [
+        "experience_bottle"
+    ],
+    "tutorial": [
+        "Alcanza el nivel 25 de experiencia"
+    ],
+    "proTip": "El nivel 30 es el máximo requerido para los encantamientos de mesa estándar.",
+    "edition": "bedrock"
+};
