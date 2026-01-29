30f148659c3b2bb6ded26823c2103909d305549f
SimpleStew007 <daniellopezgerardo02e@gmail.com>
2026-01-28 17:23:19 -0800

    Auto-Wiki: ¡A Sembrar! (bedrock)

---
+++ logros/bedrock/¡a_sembrar!.js
@@ -0,0 +1,16 @@
+window.logroData = {
+    "name": "¡A Sembrar!",
+    "icono": "wheat",
+    "categoria": "Agricultura",
+    "inventoryNed": [
+        "Semillas de Trigo",
+        "Azo de Madera"
+    ],
+    "tutorial": [
+        "Crea una Azo (hoe) de cualquier material.",
+        "Busca una fuente de agua.",
+        "Ara la tierra cerca del agua y planta las semillas que encuentres rompiendo césped."
+    ],
+    "proTip": "El agua fertiliza la tierra hasta 4 bloques de distancia. Necesitas trigo para hacer pan o criar animales.",
+    "edition": "bedrock"
+};
