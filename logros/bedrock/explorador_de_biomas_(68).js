783d553015922be4f0a523ed1a930a37d63351c3
SimpleStew007 <daniellopezgerardo02e@gmail.com>
2026-01-28 17:26:29 -0800

    Auto-Wiki: Explorador de Biomas (68) (bedrock)

---
+++ logros/bedrock/explorador_de_biomas_(68).js
@@ -0,0 +1,13 @@
+window.logroData = {
+    "name": "Explorador de Biomas (68)",
+    "icono": "tree-pine",
+    "categoria": "Viaje",
+    "inventoryNed": [
+        "map"
+    ],
+    "tutorial": [
+        "Descubre un bioma de Nieve y Hielo"
+    ],
+    "proTip": "Los strays (esqueletos de hielo) son comunes aquí.",
+    "edition": "bedrock"
+};
