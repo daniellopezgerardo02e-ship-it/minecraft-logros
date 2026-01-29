11209af331602e52356016c894f08075341103ab
SimpleStew007 <daniellopezgerardo02e@gmail.com>
2026-01-28 17:26:05 -0800

    Auto-Wiki: Llora Diamantes (27) (bedrock)

---
+++ logros/bedrock/llora_diamantes_(27).js
@@ -0,0 +1,14 @@
+window.logroData = {
+    "name": "Llora Diamantes (27)",
+    "icono": "gem",
+    "categoria": "Riqueza",
+    "inventoryNed": [
+        "diamond",
+        "emerald"
+    ],
+    "tutorial": [
+        "Acumula 64 diamantes en tu inventario"
+    ],
+    "proTip": "El comercio con aldeanos es una fuente sostenible de esmeraldas.",
+    "edition": "bedrock"
+};
