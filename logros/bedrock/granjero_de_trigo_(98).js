20d374a0474de960bed7bb101b815d5f16d69542
SimpleStew007 <daniellopezgerardo02e@gmail.com>
2026-01-28 17:26:45 -0800

    Auto-Wiki: Granjero de Trigo (98) (bedrock)

---
+++ logros/bedrock/granjero_de_trigo_(98).js
@@ -0,0 +1,13 @@
+window.logroData = {
+    "name": "Granjero de Trigo (98)",
+    "icono": "wheat",
+    "categoria": "Agricultura",
+    "inventoryNed": [
+        "bread"
+    ],
+    "tutorial": [
+        "Cosecha 100 unidades de trigo"
+    ],
+    "proTip": "El pan es una fuente eficiente de alimento.",
+    "edition": "bedrock"
+};
