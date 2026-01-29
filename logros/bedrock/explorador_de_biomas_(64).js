243f2262ed46339f61719792d024dfdb993b3ff1
SimpleStew007 <daniellopezgerardo02e@gmail.com>
2026-01-28 17:26:26 -0800

    Auto-Wiki: Explorador de Biomas (64) (bedrock)

---
+++ logros/bedrock/explorador_de_biomas_(64).js
@@ -0,0 +1,13 @@
+window.logroData = {
+    "name": "Explorador de Biomas (64)",
+    "icono": "tree-pine",
+    "categoria": "Viaje",
+    "inventoryNed": [
+        "map"
+    ],
+    "tutorial": [
+        "Descubre un bioma de Jungla"
+    ],
+    "proTip": "Las junglas son densas y a menudo contienen templos.",
+    "edition": "bedrock"
+};
