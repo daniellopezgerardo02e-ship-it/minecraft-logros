fc44399e471ad693111d322a71bad29237196d87
SimpleStew007 <daniellopezgerardo02e@gmail.com>
2026-01-28 17:26:42 -0800

    Auto-Wiki: Coleccionista de Flores I (92) (bedrock)

---
+++ logros/bedrock/coleccionista_de_flores_i_(92).js
@@ -0,0 +1,13 @@
+window.logroData = {
+    "name": "Coleccionista de Flores I (92)",
+    "icono": "flower",
+    "categoria": "Estética",
+    "inventoryNed": [
+        "dandelion"
+    ],
+    "tutorial": [
+        "Recolecta una Diente de León"
+    ],
+    "proTip": "Se usa para hacer tinte amarillo.",
+    "edition": "bedrock"
+};
