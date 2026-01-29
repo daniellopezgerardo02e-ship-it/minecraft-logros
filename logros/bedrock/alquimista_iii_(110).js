db4231377cb5430b1840b3ea91384d2adff6411e
SimpleStew007 <daniellopezgerardo02e@gmail.com>
2026-01-28 17:26:52 -0800

    Auto-Wiki: Alquimista III (110) (bedrock)

---
+++ logros/bedrock/alquimista_iii_(110).js
@@ -0,0 +1,13 @@
+window.logroData = {
+    "name": "Alquimista III (110)",
+    "icono": "vial",
+    "categoria": "Pociones",
+    "inventoryNed": [
+        "magma_cream"
+    ],
+    "tutorial": [
+        "Crea una poción de Resistencia al Fuego"
+    ],
+    "proTip": "Esencial para el Nether. Requiere Crema de Magma.",
+    "edition": "bedrock"
+};
