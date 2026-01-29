f3fa89bf4d6895e35d3e7d5cbb445f82f6f01bb1
SimpleStew007 <daniellopezgerardo02e@gmail.com>
2026-01-28 17:26:37 -0800

    Auto-Wiki: Enemigo de los Zombis (84) (bedrock)

---
+++ logros/bedrock/enemigo_de_los_zombis_(84).js
@@ -0,0 +1,13 @@
+window.logroData = {
+    "name": "Enemigo de los Zombis (84)",
+    "icono": "sword",
+    "categoria": "Combate",
+    "inventoryNed": [
+        "rotten_flesh"
+    ],
+    "tutorial": [
+        "Mata 100 Zombis"
+    ],
+    "proTip": "Los Zombis arden durante el día si no están cubiertos.",
+    "edition": "bedrock"
+};
