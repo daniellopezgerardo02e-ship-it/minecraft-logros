29465270c9ca820c1297806d9f60deb1cf53f7f0
SimpleStew007 <daniellopezgerardo02e@gmail.com>
2026-01-28 17:26:39 -0800

    Auto-Wiki: Enemigo de los Creepers (86) (bedrock)

---
+++ logros/bedrock/enemigo_de_los_creepers_(86).js
@@ -0,0 +1,13 @@
+window.logroData = {
+    "name": "Enemigo de los Creepers (86)",
+    "icono": "leaf",
+    "categoria": "Combate",
+    "inventoryNed": [
+        "gunpowder"
+    ],
+    "tutorial": [
+        "Mata 100 Creepers"
+    ],
+    "proTip": "Golpea y retrocede (hit and run) para evitar la explosión.",
+    "edition": "bedrock"
+};
