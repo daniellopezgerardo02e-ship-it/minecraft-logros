1156cec8358dc70cd34625f93e5c6429ff18a25e
SimpleStew007 <daniellopezgerardo02e@gmail.com>
2026-01-28 17:23:18 -0800

    Auto-Wiki: Cazador de Mobs (bedrock)

---
+++ logros/bedrock/cazador_de_mobs.js
@@ -0,0 +1,14 @@
+window.logroData = {
+    "name": "Cazador de Mobs",
+    "icono": "sword",
+    "categoria": "Combate",
+    "inventoryNed": [
+        "Carne Podrida"
+    ],
+    "tutorial": [
+        "Espera a que caiga la noche o explora cuevas.",
+        "Elimina a cualquier criatura hostil (Zombie, Esqueleto, Araña)."
+    ],
+    "proTip": "Los esqueletos sueltan huesos, que puedes convertir en harina de hueso, útil para acelerar el crecimiento de tus cultivos.",
+    "edition": "bedrock"
+};
