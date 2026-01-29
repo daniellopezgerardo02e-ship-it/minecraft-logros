e3b5f24eda8029be246404713b745bfcde16d7c1
SimpleStew007 <daniellopezgerardo02e@gmail.com>
2026-01-28 17:24:15 -0800

    Auto-Wiki: ¡Diamantes! (bedrock)

---
+++ logros/bedrock/¡diamantes!.js
@@ -0,0 +1,16 @@
+window.logroData = {
+    "name": "¡Diamantes!",
+    "icono": "diamond",
+    "categoria": "Minerales Raros",
+    "inventoryNed": [
+        "Pico de Hierro",
+        "Diamante"
+    ],
+    "tutorial": [
+        "1. Mina hasta la capa Y=11 o inferior (la capa -59 es la más común en Bedrock).",
+        "2. Busca el mineral azul brillante.",
+        "3. Extrae el mineral usando un Pico de Hierro o superior."
+    ],
+    "proTip": "Minar en tiras largas (strip mining) en las capas bajas es la forma más segura de encontrar Diamantes.",
+    "edition": "bedrock"
+};
