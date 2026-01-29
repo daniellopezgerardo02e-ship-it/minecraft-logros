8b6550a12381736c820738ff5e0afd1bde883599
SimpleStew007 <daniellopezgerardo02e@gmail.com>
2026-01-28 17:26:07 -0800

    Auto-Wiki: A través del Portal (30) (bedrock)

---
+++ logros/bedrock/a_través_del_portal_(30).js
@@ -0,0 +1,16 @@
+window.logroData = {
+    "name": "A través del Portal (30)",
+    "icono": "rabbit",
+    "categoria": "Agricultura",
+    "inventoryNed": [
+        "carrot_on_a_stick",
+        "pig_spawn_egg"
+    ],
+    "tutorial": [
+        "Mina el oro del Nether",
+        "Encuentra un cerdo y ponle una silla de montar",
+        "Contrólalo con la caña y zanahoria"
+    ],
+    "proTip": "Los cerdos se detienen al cruzar el portal.",
+    "edition": "bedrock"
+};
