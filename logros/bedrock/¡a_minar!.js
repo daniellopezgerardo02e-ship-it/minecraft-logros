b0fb02244d734bc888317e597651115a811b66e2
SimpleStew007 <daniellopezgerardo02e@gmail.com>
2026-01-28 17:22:23 -0800

    Auto-Wiki: ¡A Minar! (bedrock)

---
+++ logros/bedrock/¡a_minar!.js
@@ -0,0 +1,14 @@
+window.logroData = {
+    "name": "¡A Minar!",
+    "icono": "pickaxe",
+    "categoria": "Herramientas",
+    "inventoryNed": [
+        "Pico de Madera (1)"
+    ],
+    "tutorial": [
+        "Crea 4 palos usando 2 tablas.",
+        "Usa 3 tablas y 2 palos en la Mesa de Crafteo para crear un pico."
+    ],
+    "proTip": "El pico de madera te permite minar piedra. ¡No intentes minar piedra sin él!",
+    "edition": "bedrock"
+};
