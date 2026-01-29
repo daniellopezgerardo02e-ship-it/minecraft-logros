ab84d5ab243f24b76d97f7ad6ab2fd28d82840ad
SimpleStew007 <daniellopezgerardo02e@gmail.com>
2026-01-28 17:24:14 -0800

    Auto-Wiki: Volcador de Vacas (bedrock)

---
+++ logros/bedrock/volcador_de_vacas.js
@@ -0,0 +1,15 @@
+window.logroData = {
+    "name": "Volcador de Vacas",
+    "icono": "wallet",
+    "categoria": "Recursos Animales",
+    "inventoryNed": [
+        "Espada",
+        "Cuero"
+    ],
+    "tutorial": [
+        "1. Encuentra una Vaca (o Caballo, Llama o Conejo).",
+        "2. Derrótala para obtener Cuero."
+    ],
+    "proTip": "El cuero es necesario para craftear Libros, los cuales son esenciales para Encantamientos.",
+    "edition": "bedrock"
+};
