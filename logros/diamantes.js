35e076be28c07370c8551155d2205bf80bf2b3c9
SimpleStew007 <daniellopezgerardo02e@gmail.com>
2026-01-28 18:02:40 -0800

    Build: diamantes.js

---
+++ logros/diamantes.js
@@ -0,0 +1,8 @@
+registrarLogro({
+    id: "diamante_encontrado",
+    titulo: "Diamantes!",
+    descripcion: "Consigue o pica un diamante con un pico de hierro o superior.",
+    tipo: "Minería",
+    requisitos: "Encontrar el primer diamante.",
+    ediciones: ["java", "bedrock"]
+});
