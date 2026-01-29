075817611b72d427e1a63d4d797e3fc762491f38
SimpleStew007 <daniellopezgerardo02e@gmail.com>
2026-01-28 17:22:21 -0800

    Auto-Wiki: Talando Madera (bedrock)

---
+++ logros/bedrock/talando_madera.js
@@ -0,0 +1,14 @@
+window.logroData = {
+    "name": "Talando Madera",
+    "icono": "axe",
+    "categoria": "Básico",
+    "inventoryNed": [
+        "Tronco de cualquier tipo (1)"
+    ],
+    "tutorial": [
+        "Localiza un árbol.",
+        "Mantén presionado el botón de ataque/minado para obtener el tronco."
+    ],
+    "proTip": "Convierte los troncos en tablas inmediatamente para tener material de crafteo.",
+    "edition": "bedrock"
+};
