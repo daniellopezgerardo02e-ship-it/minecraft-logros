829caa8f5fc4e11015f839814f81da6607532a01
SimpleStew007 <daniellopezgerardo02e@gmail.com>
2026-01-28 17:26:13 -0800

    Auto-Wiki: Dominio Acuático (41) (bedrock)

---
+++ logros/bedrock/dominio_acuático_(41).js
@@ -0,0 +1,13 @@
+window.logroData = {
+    "name": "Dominio Acuático (41)",
+    "icono": "ship",
+    "categoria": "Viaje",
+    "inventoryNed": [
+        "boat"
+    ],
+    "tutorial": [
+        "Viaja 5000 bloques usando un bote"
+    ],
+    "proTip": "Los botes son sorprendentemente rápidos en el hielo azul.",
+    "edition": "bedrock"
+};
