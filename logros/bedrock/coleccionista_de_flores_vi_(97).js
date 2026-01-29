95d13f965e34d9d12471205a0dd1bd8a57cc8047
SimpleStew007 <daniellopezgerardo02e@gmail.com>
2026-01-28 17:26:45 -0800

    Auto-Wiki: Coleccionista de Flores VI (97) (bedrock)

---
+++ logros/bedrock/coleccionista_de_flores_vi_(97).js
@@ -0,0 +1,13 @@
+window.logroData = {
+    "name": "Coleccionista de Flores VI (97)",
+    "icono": "flower",
+    "categoria": "Estética",
+    "inventoryNed": [
+        "sunflower"
+    ],
+    "tutorial": [
+        "Recolecta un Girasol"
+    ],
+    "proTip": "Siempre apunta hacia el Este.",
+    "edition": "bedrock"
+};
