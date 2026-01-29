d4380b1e6cf3b777fdb6184fb14434030f93cf8f
SimpleStew007 <daniellopezgerardo02e@gmail.com>
2026-01-28 17:24:17 -0800

    Auto-Wiki: Hacia el Nether (bedrock)

---
+++ logros/bedrock/hacia_el_nether.js
@@ -0,0 +1,16 @@
+window.logroData = {
+    "name": "Hacia el Nether",
+    "icono": "portal",
+    "categoria": "Dimensiones",
+    "inventoryNed": [
+        "Obsidiana (10-14)",
+        "Pedernal y Acero"
+    ],
+    "tutorial": [
+        "1. Craftea un marco de Portal del Nether (mínimo 4x5 de exterior, 2x3 de interior).",
+        "2. Enciende el interior usando Pedernal y Acero (Flint and Steel).",
+        "3. Entra al portal."
+    ],
+    "proTip": "El Portal del Nether es la forma más rápida de viajar en el mundo principal (1 bloque en el Nether equivale a 8 bloques en la superficie).",
+    "edition": "bedrock"
+};
