d8ee7ee76b6ab3bd17df5db69d73d3ed327c98ff
SimpleStew007 <daniellopezgerardo02e@gmail.com>
2026-01-28 17:26:12 -0800

    Auto-Wiki: Maestro del Enlace (39) (bedrock)

---
+++ logros/bedrock/maestro_del_enlace_(39).js
@@ -0,0 +1,15 @@
+window.logroData = {
+    "name": "Maestro del Enlace (39)",
+    "icono": "compass",
+    "categoria": "Navegación",
+    "inventoryNed": [
+        "compass",
+        "lodestone"
+    ],
+    "tutorial": [
+        "Crea una Magnetita",
+        "Haz clic derecho con una brújula sobre ella para enlazarla"
+    ],
+    "proTip": "La brújula enlazada siempre apunta a la Magnetita, útil en el Nether o End.",
+    "edition": "bedrock"
+};
