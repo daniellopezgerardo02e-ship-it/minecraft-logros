7b3cae115ff6eff083c5ab5a6f271c7fc7c999ed
SimpleStew007 <daniellopezgerardo02e@gmail.com>
2026-01-28 18:02:40 -0800

    Build: pesca.js

---
+++ logros/pesca.js
@@ -0,0 +1,8 @@
+registrarLogro({
+    id: "pesca_clasica",
+    titulo: "Pescador en la Esquina",
+    descripcion: "Pesca un pez. (Clásico de Bedrock/Consola).",
+    tipo: "Actividad Pasiva",
+    requisitos: "Pescar un objeto usando la caña de pescar.",
+    ediciones: ["bedrock"]
+});
