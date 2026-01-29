25a05d204d0681efc2e6bf9dd5d79728985dddd0
SimpleStew007 <daniellopezgerardo02e@gmail.com>
2026-01-28 17:26:37 -0800

    Auto-Wiki: Maestro Minero VIII (83) (bedrock)

---
+++ logros/bedrock/maestro_minero_viii_(83).js
@@ -0,0 +1,13 @@
+window.logroData = {
+    "name": "Maestro Minero VIII (83)",
+    "icono": "pickaxe",
+    "categoria": "Minería",
+    "inventoryNed": [
+        "ancient_debris"
+    ],
+    "tutorial": [
+        "Mina 10 minerales de Escombros Ancestrales"
+    ],
+    "proTip": "Usa TNT para minar en el Nether en el nivel Y=15.",
+    "edition": "bedrock"
+};
