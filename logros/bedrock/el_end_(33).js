f44ac981b855d76a7ec854943c9196bcfca17a32
SimpleStew007 <daniellopezgerardo02e@gmail.com>
2026-01-28 17:26:08 -0800

    Auto-Wiki: El End (33) (bedrock)

---
+++ logros/bedrock/el_end_(33).js
@@ -0,0 +1,13 @@
+window.logroData = {
+    "name": "El End (33)",
+    "icono": "moon",
+    "categoria": "Final",
+    "inventoryNed": [
+        "end_stone"
+    ],
+    "tutorial": [
+        "Entra al portal del End"
+    ],
+    "proTip": "No mires directamente a los Endermen.",
+    "edition": "bedrock"
+};
