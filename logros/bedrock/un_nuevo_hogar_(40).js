38aab6bd5f460247bca02cac2a3a5a47ac85a31c
SimpleStew007 <daniellopezgerardo02e@gmail.com>
2026-01-28 17:26:12 -0800

    Auto-Wiki: Un Nuevo Hogar (40) (bedrock)

---
+++ logros/bedrock/un_nuevo_hogar_(40).js
@@ -0,0 +1,14 @@
+window.logroData = {
+    "name": "Un Nuevo Hogar (40)",
+    "icono": "home",
+    "categoria": "Construcción",
+    "inventoryNed": [
+        "bed",
+        "chest"
+    ],
+    "tutorial": [
+        "Construye una casa funcional de 5x5 con cama y cofre"
+    ],
+    "proTip": "Asegúrate de iluminar el interior para evitar que aparezcan mobs.",
+    "edition": "bedrock"
+};
