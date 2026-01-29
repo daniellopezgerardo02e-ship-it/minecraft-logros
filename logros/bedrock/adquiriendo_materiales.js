c1a97b4d4e934d7121e67704cfc31f2822680bd8
SimpleStew007 <daniellopezgerardo02e@gmail.com>
2026-01-28 17:22:24 -0800

    Auto-Wiki: Adquiriendo Materiales (bedrock)

---
+++ logros/bedrock/adquiriendo_materiales.js
@@ -0,0 +1,14 @@
+window.logroData = {
+    "name": "Adquiriendo Materiales",
+    "icono": "hammer",
+    "categoria": "Herramientas",
+    "inventoryNed": [
+        "Pico de Piedra (1)"
+    ],
+    "tutorial": [
+        "Mina 3 adoquines (con el pico de madera).",
+        "Combina 3 adoquines y 2 palos en la Mesa de Crafteo."
+    ],
+    "proTip": "El pico de piedra es la herramienta mínima necesaria para minar hierro y otros minerales importantes.",
+    "edition": "bedrock"
+};
