8671d2ddc9b1c09603f8ce00867f61d94e91c686
SimpleStew007 <daniellopezgerardo02e@gmail.com>
2026-01-28 17:26:51 -0800

    Auto-Wiki: Alquimista II (109) (bedrock)

---
+++ logros/bedrock/alquimista_ii_(109).js
@@ -0,0 +1,13 @@
+window.logroData = {
+    "name": "Alquimista II (109)",
+    "icono": "vial",
+    "categoria": "Pociones",
+    "inventoryNed": [
+        "ghast_tear"
+    ],
+    "tutorial": [
+        "Crea una poción de Regeneración"
+    ],
+    "proTip": "Requiere una lágrima de Ghast.",
+    "edition": "bedrock"
+};
