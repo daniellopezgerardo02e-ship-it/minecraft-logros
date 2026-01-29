27e2f7eed4e3ae745b42c5fcd26927c87685e092
SimpleStew007 <daniellopezgerardo02e@gmail.com>
2026-01-28 17:26:49 -0800

    Auto-Wiki: El Pescador III (104) (bedrock)

---
+++ logros/bedrock/el_pescador_iii_(104).js
@@ -0,0 +1,13 @@
+window.logroData = {
+    "name": "El Pescador III (104)",
+    "icono": "fishing-rod",
+    "categoria": "Pesca",
+    "inventoryNed": [
+        "pufferfish"
+    ],
+    "tutorial": [
+        "Pesca 100 peces Crudos"
+    ],
+    "proTip": "Algunos peces son venenosos (pez globo).",
+    "edition": "bedrock"
+};
