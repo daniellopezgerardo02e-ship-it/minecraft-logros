c5a350e4a1da1c678633fd8121a1aa26f1ed57e9
SimpleStew007 <daniellopezgerardo02e@gmail.com>
2026-01-28 17:26:28 -0800

    Auto-Wiki: Explorador de Biomas (66) (bedrock)

---
+++ logros/bedrock/explorador_de_biomas_(66).js
@@ -0,0 +1,13 @@
+window.logroData = {
+    "name": "Explorador de Biomas (66)",
+    "icono": "tree-pine",
+    "categoria": "Viaje",
+    "inventoryNed": [
+        "map"
+    ],
+    "tutorial": [
+        "Descubre un bioma de Montañas Rocosas"
+    ],
+    "proTip": "Ideal para encontrar carbón y hierro en la superficie.",
+    "edition": "bedrock"
+};
