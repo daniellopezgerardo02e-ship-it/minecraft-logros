c20e3ba32461a5a5d24f8b02e880f3e806e1f195
SimpleStew007 <daniellopezgerardo02e@gmail.com>
2026-01-28 17:26:36 -0800

    Auto-Wiki: Maestro Minero VII (82) (bedrock)

---
+++ logros/bedrock/maestro_minero_vii_(82).js
@@ -0,0 +1,13 @@
+window.logroData = {
+    "name": "Maestro Minero VII (82)",
+    "icono": "pickaxe",
+    "categoria": "Minería",
+    "inventoryNed": [
+        "emerald"
+    ],
+    "tutorial": [
+        "Mina 50 minerales de Esmeralda"
+    ],
+    "proTip": "Solo se encuentran en los biomas de montañas, o a través del comercio.",
+    "edition": "bedrock"
+};
