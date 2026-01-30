60c657f296ed6de161005a550028e7c9e3ffec3c
SimpleStew007 <daniellopezgerardo02e@gmail.com>
2026-01-29 19:02:38 -0800

    Clean

--- logros/dragon.js
+++
@@ -1,9 +0,0 @@
-/** Este archivo simula la estructura de un archivo de detalle que se cargaría para una vista de logro individual. */
-window.logroData = {
-    id: 'liberando_end',
-    name: 'Liberando al End',
-    desc: 'Mata al temido Dragón del End y escapa de la dimensión.',
-    type: 'Java/Bedrock',
-    prerequisito: 'Viaje al End',
-    guia: 'Destruye los cristales sanadores primero. ¡Usa arcos o pociones de daño instantáneo!'
-};
