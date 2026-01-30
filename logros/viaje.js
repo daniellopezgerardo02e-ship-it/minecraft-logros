a555d5b39648593cf86070ff511ec23f78346dd9
SimpleStew007 <daniellopezgerardo02e@gmail.com>
2026-01-29 19:02:43 -0800

    Clean

--- logros/viaje.js
+++
@@ -1,9 +0,0 @@
-/** Este archivo simula la estructura de un archivo de detalle que se cargaría para una vista de logro individual. */
-window.logroData = {
-    id: 'viaje_al_end',
-    name: 'Viaje al End',
-    desc: 'Encuentra y activa el portal del End (generalmente en una fortaleza).',
-    type: 'Java',
-    prerequisito: 'Ojos de Ender',
-    guia: 'Usa los Ojos de Ender en la superficie para localizar la fortaleza subterránea donde reside el portal.'
-};
