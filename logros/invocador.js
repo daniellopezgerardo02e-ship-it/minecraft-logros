634ba1650fa40c1b97c24328bd469151fdb198cf
SimpleStew007 <daniellopezgerardo02e@gmail.com>
2026-01-29 19:02:39 -0800

    Clean

--- logros/invocador.js
+++
@@ -1,8 +0,0 @@
-registrarLogro({
-    id: "invoca_wither",
-    titulo: "La Oscuridad a tu Voluntad",
-    descripcion: "Construye y observa la invocación del Wither. (Este es un Avance de Java).",
-    tipo: "Jefe Opcional",
-    requisitos: "Usar 4 bloques de arena de almas y 3 calaveras de Wither Skeleton.",
-    ediciones: ["java"]
-});
